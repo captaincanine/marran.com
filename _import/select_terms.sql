df6a8263d0f9585f7b8002fab8be16deb63f82bf
Keith Marran <pelosi@usa.net>
2011-02-13 22:46:05 -0500

    Successfully removing _import

--- _import/select_terms.sql
+++
@@ -1,8 +0,0 @@
-select tn.nid, td.tid, td.name,
-(select tp.name from term_hierarchy inner join term_data tp on tp.tid = term_hierarchy.parent where td.tid = term_hierarchy.tid) as parent
-from term_data td 
-inner join term_node tn on td.tid = tn.tid;
-
---select * from node where title like '%chicken%';
-
---select * from term_hierarchy where tid in (513, 519);
