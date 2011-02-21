ffd89f1ed8032188d1be6b2e704ed713d2139ba2
Keith Marran <pelosi@usa.net>
2011-02-21 12:20:46 -0500

    Adjusted Facebook feed pcitures and style

--- js/facebook/status-updates.js
+++ js/facebook/status-updates.js
@@ -45,7 +45,7 @@ function showPosts(posts) {
 					content = '<div class="feed-entry clearfix">';
 					if (entry.message != null) { content += '<div class="message">' + entry.message + '</div>'};
 					content += '<div class="feed-video clearfix">';
-					content += '<div class="picture"><a href="' + entry.link + '" target="_blank"><img src="' + entry.picture + '" /></a></div>';
+					if (entry.picture != null) { content += '<div class="picture"><a href="' + entry.link + '" target="_blank"><img src="' + entry.picture + '" /></a></div>'; }
 					content += '<div class="name"><a href="' + entry.link + '" target="_blank">' + entry.name + '</a></div>';
 					if (entry.description) { content += '<div class="description">' + entry.description + '</div>'; }
 					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
