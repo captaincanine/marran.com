f0b8962a0b95de1b0881070d005f9e49050aebb8
Keith Marran <pelosi@usa.net>
2011-03-04 08:05:52 -0500

    fixed issues with outbound links an image popups

--- js/link-helper.js
+++ js/link-helper.js
@@ -5,9 +5,8 @@ $(document).ready( function () {
 		if (a.hostname && a.hostname !== location.hostname) {
 			$(a).not(".noAutoIcon").addClass("offSite");
 			$(a).not(".noAutoLink").attr('target','_blank').bind('click keypress', function(event) {
-				_gat._getTrackerByName()._trackEvent(category, action);
-				setTimeout('window.open("' + link.href + '")', 100);
-				return false;
+				_gat._getTrackerByName()._trackEvent("outbound", this.href);
+				return true;
 			});
 		};
 	});
