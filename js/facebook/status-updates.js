c2a467b8670071901b8ad0e06a322e67bed7dd8d
Keith Marran <pelosi@usa.net>
2012-09-26 14:24:08 -0400

    Move to Twitter Bootstrap

--- js/facebook/status-updates.js
+++ js/facebook/status-updates.js
@@ -16,39 +16,39 @@ function showPosts(posts) {
 			switch (entry.type) {
 				case 'status':
 					content = '<div class="feed-entry clearfix">';
-					if (entry.message != null) { content += '<div class="message">' + entry.message + '</div>'};
 					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
+					if (entry.message != null) { content += '<div class="message">' + entry.message + '</div>'};
 					content += '</div>';
 					$('#feed').append($(content));
 					break;
 				case 'photo':
 					content = '<div class="feed-entry clearfix">';
+					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
 					if (entry.message != null) { content += '<div class="message">' + entry.message + '</div>'};
 					content += '<div class="picture"><a href="' + entry.link + '" target="_blank"><img src="' + entry.picture + '" /></a></div>';
-					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
 					content += '</div>';
 					$('#feed').append($(content));
 					break;
 				case 'video':
 					content = '<div class="feed-entry clearfix">';
+					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
 					if (entry.message != null) { content += '<div class="message">' + entry.message + '</div>'};
 					content += '<div class="feed-video clearfix">';
 					content += '<div class="picture"><a href="' + entry.link + '" target="_blank"><img src="' + entry.picture + '" /></a></div>';
 					content += '<div class="name"><a href="' + entry.link + '" target="_blank">' + entry.name + '</a></div>';
 					if (entry.description) { content += '<div class="description">' + entry.description + '</div>'; }
-					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
 					content += '</div>';
 					content += '</div>';
 					$('#feed').append($(content));
 					break;
 				case 'link':
 					content = '<div class="feed-entry clearfix">';
+					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
 					if (entry.message != null) { content += '<div class="message">' + entry.message + '</div>'};
 					content += '<div class="feed-video clearfix">';
 					if (entry.picture != null) { content += '<div class="picture"><a href="' + entry.link + '" target="_blank"><img src="' + entry.picture + '" /></a></div>'; }
 					content += '<div class="name"><a href="' + entry.link + '" target="_blank">' + entry.name + '</a></div>';
 					if (entry.description) { content += '<div class="description">' + entry.description + '</div>'; }
-					content += '<div class="timeago">' + $.timeago(entry.created_time) + '</div>';
 					content += '</div>';
 					content += '</div>';
 					$('#feed').append($(content));
